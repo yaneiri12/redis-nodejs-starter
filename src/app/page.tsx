96448b25e0c3c4471f10d74a75b603c9eb2f9bdd
Will Johnston <will-github@wwj.dev>
2024-05-21 18:52:39 -0700

    more styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,9 +48,19 @@ export default async function Home() {
       <section className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           This is your&nbsp;
-          <code className="font-mono font-bold">
-            {ordinalSuffix(visits)} visit according
-          </code>
+          <code className="font-bold">{ordinalSuffix(visits)}&nbsp;</code>
+          visit.&nbsp;
+          <a
+            href="https://redis.io/learn/develop/node/nodecrashcourse/sessionstorage?utm_source=redis-node-starter&utm_campaign=redis-node-starter"
+            className="group px-2 transition-colors text-[#DCFF1E] rounded-[5px] text-sm active:text-white"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            See how we know{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </a>
         </p>
       </section>
 
