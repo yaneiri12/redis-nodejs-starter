e25fbef183d1f07d928a96896359279237b65088
Will Johnston <will-github@wwj.dev>
2024-05-23 11:46:33 -0700

    light and dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,13 +16,13 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* Visits */}
       <section className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-[#163341] text-white pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 lg:border lg:p-4">
           This is your&nbsp;
           <code className="font-bold">{appendOrdinalSuffix(visits)}&nbsp;</code>
           visit.&nbsp;
           <a
             href="https://redis.io/learn/develop/node/nodecrashcourse/sessionstorage?utm_source=redis-node-starter&utm_campaign=redis-node-starter"
-            className="group px-2 transition-colors text-[#DCFF1E] rounded-[5px] text-sm active:text-white"
+            className="group px-2 transition-colors hover:text-[#DCFF1E] text-sm"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -56,14 +56,14 @@ export default async function Home() {
       <section className="py-12 contain-layout">
         <div className="max-w-7xl mx-auto px-4 lg:px-8 py-16">
           <div className="mx-auto flex w-100 flex-col items-center gap-8 text-center">
-            <h2 className="capitalize text-5xl font-sans text-display-xs sm:text-display-lg text-white">
+            <h2 className="capitalize text-5xl font-sans text-display-xs sm:text-display-lg">
               Try some sample data
             </h2>
             <p className="text-xl text-center font-mono">
               Click a button to load sample data, then open{" "}
               <a
                 href="https://redis.io/insight?utm_source=redis-node-starter&utm_campaign=redis-node-starter#insight-form"
-                className="text-[#DCFF1E] active:text-white"
+                className="font-bold hover:font-normal dark:hover:text-[#DCFF1E]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -73,7 +73,7 @@ export default async function Home() {
             </p>
             <div className="grid lg:grid-cols-4">
               <form action={loadProducts} className="px-2 py-4">
-                <button className="inline-flex items-center justify-center gap-2 text-center transition-colors w-full h-11 whitespace-nowrap font-normal font-mono bg-[#DCFF1E] text-[#091A23] rounded-[5px] border border-[#5C707A] px-8 py-[14px] text-sm active:bg-[#163341] active:text-white sm:w-fit">
+                <button className="inline-flex items-center justify-center gap-2 text-center transition-colors w-full h-11 whitespace-nowrap font-normal font-mono bg-[#DCFF1E] text-[#091A23] rounded-[5px] border border-[#5C707A] px-8 py-[14px] text-sm hover:bg-[#163341] hover:text-white sm:w-fit">
                   Load products
                 </button>
               </form>
@@ -89,7 +89,7 @@ export default async function Home() {
       <section className="py-12 contain-layout">
         <div className="max-w-7xl mx-auto px-4 lg:px-8 py-16">
           <div className="mx-auto flex w-100 flex-col items-center gap-8 text-center">
-            <h2 className="capitalize text-5xl font-sans text-display-xs sm:text-display-lg text-white">
+            <h2 className="capitalize text-5xl font-sans text-display-xs sm:text-display-lg">
               Learn More
             </h2>
             <p className="text-xl text-center font-mono">
@@ -99,11 +99,11 @@ export default async function Home() {
             <div className="pt-10 mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left font-mono">
               <a
                 href="https://redis.io/docs/latest?utm_source=redis-node-starter&utm_campaign=redis-node-starter"
-                className="group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                className="hover:text-[#DCFF1E] group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-[#163341] hover:dark:border-neutral-700 hover:dark:bg-[#163341]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <h2 className="text-[#DCFF1E] active:text-white mb-3 text-2xl">
+                <h2 className="mb-3 text-2xl">
                   Docs{" "}
                   <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                     -&gt;
@@ -116,11 +116,11 @@ export default async function Home() {
 
               <a
                 href="https://redis.io/learn?utm_source=redis-node-starter&utm_campaign=redis-node-starter"
-                className="group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                className="hover:text-[#DCFF1E] group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-[#163341] hover:dark:border-neutral-700 hover:dark:bg-[#163341]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <h2 className="text-[#DCFF1E] active:text-white mb-3 text-2xl">
+                <h2 className="mb-3 text-2xl">
                   Learn{" "}
                   <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                     -&gt;
@@ -133,11 +133,11 @@ export default async function Home() {
 
               <a
                 href="https://redis.io/demo-center?utm_source=redis-node-starter&utm_campaign=redis-node-starter"
-                className="group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                className="hover:text-[#DCFF1E] group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-[#163341] hover:dark:border-neutral-700 hover:dark:bg-[#163341]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <h2 className="text-[#DCFF1E] active:text-white mb-3 text-2xl">
+                <h2 className="mb-3 text-2xl">
                   Demo Center{" "}
                   <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                     -&gt;
@@ -151,11 +151,11 @@ export default async function Home() {
 
               <a
                 href="https://redis.io/insight?utm_source=redis-node-starter&utm_campaign=redis-node-starter#insight-form"
-                className="group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                className="hover:text-[#DCFF1E] group rounded-lg border border-transparent px-2 py-4 transition-colors hover:border-gray-300 hover:bg-[#163341] hover:dark:border-neutral-700 hover:dark:bg-[#163341]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <h2 className="text-[#DCFF1E] active:text-white mb-3 text-2xl">
+                <h2 className="mb-3 text-2xl">
                   Redis Insight{" "}
                   <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                     -&gt;
@@ -183,7 +183,7 @@ export default async function Home() {
             </h2>
             <p className="text-[26px] font-sans text-white"></p>
             <a
-              className="inline-flex items-center justify-center gap-2 text-center transition-colors w-full h-11 whitespace-nowrap font-normal font-mono bg-[#DCFF1E] text-[#091A23] rounded-[5px] border border-[#5C707A] px-8 py-[14px] text-sm active:bg-[#163341] active:text-white sm:w-fit"
+              className="inline-flex items-center justify-center gap-2 text-center transition-colors w-full h-11 whitespace-nowrap font-normal font-mono bg-[#DCFF1E] text-[#091A23] rounded-[5px] border border-[#5C707A] px-8 py-[14px] text-sm hover:bg-[#163341] hover:text-white sm:w-fit"
               rel="noreferrer noopener"
               target="_blank"
               href="https://redis.io/try-free/"
