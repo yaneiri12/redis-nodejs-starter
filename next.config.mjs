38651c4dd0d7b082a5f3d02c077643c43e566811
Will Johnston <will-github@wwj.dev>
2024-05-30 07:34:32 -0700

    fixing next config to allow codespaces origin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "*.app.github.dev"],
+    },
+  },
+};
 
 export default nextConfig;
